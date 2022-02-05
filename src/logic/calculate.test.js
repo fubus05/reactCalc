947ca4342659386449cf6cbda524ea80ca5acdb2
IvanFD <74356123+wine-blip@users.noreply.github.com>
2022-02-05 23:47:18 +0300

    1
    1

---
+++ src/logic/calculate.test.js
@@ -0,0 +1,48 @@
+import calculate from "./calculate";
+import chai from "chai";
+
+// https://github.com/chaijs/chai/issues/469
+chai.config.truncateThreshold = 0;
+
+const expect = chai.expect;
+
+function pressButtons(buttons) {
+  const value = {};
+  buttons.forEach(button => {
+    Object.assign(value, calculate(value, button));
+  });
+  // no need to distinguish between null and undefined values
+  Object.keys(value).forEach(key => {
+    if (value[key] === null) {
+      delete value[key];
+    }
+  });
+  return value;
+}
+
+function expectButtons(buttons, expectation) {
+  expect(pressButtons(buttons)).to.deep.equal(expectation);
+}
+
+function test(buttons, expectation, only = false) {
+  const func = only ? it.only : it;
+  func(`buttons ${buttons.join(",")} -> ${JSON.stringify(expectation)}`, () => {
+    expectButtons(buttons, expectation);
+  });
+}
+
+describe("calculate", function() {
+  test(["6"], { next: "6" });
+
+  test(["6", "6"], { next: "66" });
+
+  test(["6", "+", "6"], {
+    next: "6",
+    total: "6",
+    operation: "+",
+  });
+
+  test(["6", "+", "6", "="], {
+    total: "12",
+  });
+});
